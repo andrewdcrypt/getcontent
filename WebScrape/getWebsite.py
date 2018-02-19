dc8cfb3cc001b8d13a07e7e6ddc462c33bc2b6ba
sleepyfoo <sleepyfoo@nonyabusinessbaka.com>
2018-02-18 19:57:26 -0800

    script will also now create a excel file and adds the scape data to it

--- WebScrape/getWebsite.py
+++ WebScrape/getWebsite.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import pandas as pandas
 
 weburl = 'https://coinmarketcap.com/'
 
@@ -10,9 +11,17 @@ try:
 
     count = 0
 
+    rank = []
+    cryptoName = []
+
     while count < 100:
+        rank.append(count+1)
+        cryptoName.append(get_currency_name[count].get_text())
         print ('Rank#: '+ str(count+1) + ' Name:' + str(get_currency_name[count].get_text()))
         count = count + 1
+
+    addInfo = pandas.DataFrame({'Rank #:': rank, 'Crypto Name:': cryptoName}, columns=['Rank #:', 'Crypto Name:'])
+    addInfo.to_excel('coininfo.xlsx', sheet_name='Dash', index=False)
 
 except Exception as e:
     print ("Error: " + str(e))
